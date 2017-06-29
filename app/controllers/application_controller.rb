1e8006ea58531b1485f21e677ed0df46ce6bd052
arththakkar <arththakkar@gmail.com>
2017-06-29 09:59:19 +0000

    initialising repository

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  def hello
-      render html: "ruby on rails"
-  end
+  include SessionsHelper
 end
+    
+
+
